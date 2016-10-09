f71cda0d2d3a8bf019b7c9d341265f553bf7a5b3
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-10-09 19:12:59 +0200

    add sass to meteor and add style for the components. Also add normalize css

--- client/main.js
+++ client/main.js
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import EmployeeList from './components/employee_list';
 
-
 const App = (props) => {
   return (
     <div>
